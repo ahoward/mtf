fa2ad7e6afe8242f45e77acf2ae61137ed06d82e
ahoward <ara.t.howard@gmail.com>
2023-03-06 17:33:17 -0700

    wip @ 2023-03-06T17:33:16-07:00

--- fe/src/app/layout.js
+++ fe/src/app/layout.js
@@ -1,9 +1,18 @@
 import "./globals.css";
 import util from "@/lib/util";
 
+class Emoji {
+  static dragon = "🐉";
+  static mountain = "⛰";
+  static fire = "🔥";
+  static herb = "🌿";
+  static beers = "🍻";
+  static dancer = "💃🏿";
+}
+
 export async function generateMetadata({ params }) {
   const title = "MATANUSKA FRÖNTIER TREK";
-  const description = "High around the Valley :: 🐉 + 🔥 + 🌿 + ⛰️ + 🍻 + 💃🏿";
+  const description = `High around the Valley :: ${Emoji.dragon} + ${Emoji.mountain} + ${Emoji.fire} + ${Emoji.herb} + ${Emoji.beers} + ${Emoji.dancer}`;
   const images = ["/MTF-OG.png"];
   const openGraph = { title, description, images };
   const robots = { index: true };
